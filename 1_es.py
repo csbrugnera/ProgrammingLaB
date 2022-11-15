b0087a9e04c92bbfa7cf92a9d034e71a93fdaaf5
csbrugnera <115221376+csbrugnera@users.noreply.github.com>
2022-11-15 13:14:51 +0000

    lez2

---
+++ 1_es.py
@@ -0,0 +1,14 @@
+#scrivere una funzione sum_list che sommi tutti gli elementi di una lista
+def sum_list(item):
+  somma = 0
+  for item in my_list: 
+    somma = somma + item
+    
+  return(somma)
+
+
+my_list = [1, 3, 6, 9]
+#somma = 0 non va bene qua perche solo variabili locali!
+somma = sum_list(my_list)
+print(somma)
+
